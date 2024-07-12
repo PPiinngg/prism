fa356dc6c9f68c8168d284333ea249261add48f0
Glyphli <38050295+PPiinngg@users.noreply.github.com>
2024-07-12 11:41:33 +0100

    WRITING ON PGHONE FROM TRAIN HELLO 👋

--- prism.lisp
+++ prism.lisp
@@ -1,5 +1,10 @@
+;;; [TODO]: "require-trailing-whitespace" param
 (defun line-type (str match)
   (if (< (length str) (length match))
     nil
     (string= (subseq str 0 (length match))
              match)))
+
+(let ((line "AAAATEST"))
+  (cond ((line-type line "=>"))
+        (t)))
